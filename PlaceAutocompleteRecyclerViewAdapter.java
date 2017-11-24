65c9a2916d682fb957d4aa8016e5daad2b0ad1ce
Rajeshtechgeek <rajeshtechgeek@gmail.com>
2017-11-24 16:52:10 +0530

    Create PlaceAutocompleteRecyclerViewAdapter.java

---
+++ PlaceAutocompleteRecyclerViewAdapter.java
@@ -0,0 +1,212 @@
+**
+ * Adapter that handles Autocomplete requests from the Places Geo Data Client.
+ * {@link AutocompletePrediction} results from the API are frozen and stored directly in this
+ * adapter. (See {@link AutocompletePrediction#freeze()}.)
+ */
+public class PlaceAutocompleteRecyclerViewAdapter
+        extends RecyclerView.Adapter<PlaceAutocompleteRecyclerViewAdapter.ViewHolder> implements Filterable {
+
+    private static final String TAG = "PlaceAutocomplete";
+    private static final CharacterStyle STYLE_BOLD = new StyleSpan(Typeface.NORMAL);
+    /**
+     * Current results returned by this adapter.
+     */
+    private ArrayList<AutocompletePrediction> mResultList=new ArrayList<>();
+
+    /**
+     * Handles autocomplete requests.
+     */
+    private GeoDataClient mGeoDataClient;
+
+    /**
+     * The bounds used for Places Geo Data autocomplete API requests.
+     */
+    private LatLngBounds mBounds;
+
+    /**
+     * The autocomplete filter used to restrict queries to a specific set of place types.
+     */
+    private AutocompleteFilter mPlaceFilter;
+    private Context context;
+    public SearchResultsAdapter.OnItemClickListener onItemClickListener;
+
+    /**
+     * Initializes with a resource for text rows and autocomplete query bounds.
+     *
+     * @see ArrayAdapter#ArrayAdapter(Context, int)
+     */
+    public PlaceAutocompleteRecyclerViewAdapter(Context context, GeoDataClient geoDataClient, LatLngBounds bounds, AutocompleteFilter filter) {
+        this.context = context;
+        mGeoDataClient = geoDataClient;
+        mBounds = bounds;
+        mPlaceFilter = filter;
+    }
+
+    /**
+     * Sets the bounds for all subsequent queries.
+     */
+    public void setBounds(LatLngBounds bounds) {
+        mBounds = bounds;
+    }
+
+
+
+    public AutocompletePrediction getItem(int position) {
+        return mResultList.get(position);
+    }
+
+    /**
+     * Returns the filter for the current set of autocomplete results.
+     */
+    @Override
+    public Filter getFilter() {
+        return new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence constraint) {
+                FilterResults results = new FilterResults();
+
+                // We need a separate list to store the results, since
+                // this is run asynchronously.
+                ArrayList<AutocompletePrediction> filterData = new ArrayList<>();
+
+                // Skip the autocomplete query if no constraints are given.
+                if (constraint != null) {
+                    // Query the autocomplete API for the (constraint) search string.
+                    filterData = getAutocomplete(constraint);
+                }
+
+                results.values = filterData;
+                if (filterData != null) {
+                    results.count = filterData.size();
+                } else {
+                    results.count = 0;
+                }
+
+                return results;
+            }
+
+            @Override
+            protected void publishResults(CharSequence constraint, FilterResults results) {
+
+                if (results != null && results.count > 0) {
+                    // The API returned at least one result, update the data.
+                    mResultList = (ArrayList<AutocompletePrediction>) results.values;
+                    notifyDataSetChanged();
+                } else {
+                    // The API did not return any results, invalidate the data set.
+                    notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public CharSequence convertResultToString(Object resultValue) {
+                // Override this method to display a readable result in the AutocompleteTextView
+                // when clicked.
+                if (resultValue instanceof AutocompletePrediction) {
+                    return ((AutocompletePrediction) resultValue).getFullText(null);
+                } else {
+                    return super.convertResultToString(resultValue);
+                }
+            }
+        };
+    }
+
+    /**
+     * Submits an autocomplete query to the Places Geo Data Autocomplete API.
+     * Results are returned as frozen AutocompletePrediction objects, ready to be cached.
+     * Returns an empty list if no results were found.
+     * Returns null if the API client is not available or the query did not complete
+     * successfully.
+     * This method MUST be called off the main UI thread, as it will block until data is returned
+     * from the API, which may include a network request.
+     *
+     * @param constraint Autocomplete query string
+     * @return Results from the autocomplete API or null if the query was not successful.
+     * @see GeoDataClient#getAutocompletePredictions(String, LatLngBounds, AutocompleteFilter)
+     * @see AutocompletePrediction#freeze()
+     */
+    public ArrayList<AutocompletePrediction> getAutocomplete(CharSequence constraint) {
+        Log.i(TAG, "Starting autocomplete query for: " + constraint);
+
+        // Submit the query to the autocomplete API and retrieve a PendingResult that will
+        // contain the results when the query completes.
+        Task<AutocompletePredictionBufferResponse> results =
+                mGeoDataClient.getAutocompletePredictions(constraint.toString(), mBounds,
+                        mPlaceFilter);
+        // This method should have been called off the main UI thread. Block and wait for at most
+        // 60s for a result from the API.
+        try {
+            Tasks.await(results, 60, TimeUnit.SECONDS);
+        } catch (ExecutionException | InterruptedException | TimeoutException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            AutocompletePredictionBufferResponse autocompletePredictions = results.getResult();
+
+            Log.i(TAG, "Query completed. Received " + autocompletePredictions.getCount()
+                    + " predictions.");
+
+            // Freeze the results immutable representation that can be stored safely.
+            return DataBufferUtils.freezeAndClose(autocompletePredictions);
+        } catch (RuntimeExecutionException e) {
+            // If the query did not complete successfully return null
+            Toast.makeText(context, "Error contacting API: " + e.toString(),
+                    Toast.LENGTH_SHORT).show();
+            Log.e(TAG, "Error getting autocomplete prediction API call", e);
+            return null;
+        }
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(android.R.layout.simple_expandable_list_item_2, parent, false);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+        // Sets the primary and secondary text for a row.
+        // Note that getPrimaryText() and getSecondaryText() return a CharSequence that may contain
+        // styling based on the given CharacterStyle.
+
+        AutocompletePrediction item = getItem(position);
+
+        holder.textView1.setText(item.getPrimaryText(STYLE_BOLD));
+        holder.textView2.setText(item.getSecondaryText(STYLE_BOLD));
+    }
+
+    /**
+     * Returns the number of results received in the last autocomplete query.
+     */
+    @Override
+    public int getItemCount() {
+        return mResultList.size();
+    }
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        private TextView textView1,textView2;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            this.itemView.setOnClickListener(this);
+             textView1 = itemView.findViewById(android.R.id.text1);
+             textView2 = itemView.findViewById(android.R.id.text2);
+
+        }
+
+        @Override
+        public void onClick(View view) {
+            int position = getAdapterPosition();
+            onItemClickListener.onRecyclerItemClick(view,position);
+        }
+    }
+
+    public void setOnItemClickListener(SearchResultsAdapter.OnItemClickListener clickListener) {
+        this.onItemClickListener = clickListener;
+    }
+}
